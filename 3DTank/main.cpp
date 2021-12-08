f4b7ee012bd937244f0f9bbfe02cde78edb0874b
Azuma <g021g1256@g.neec.ac.jp>
2021-12-08 23:16:03 +0900

    描画テスト

--- 3DTank/main.cpp
+++ 3DTank/main.cpp
@@ -1,5 +1,26 @@
+#include <GSgame.h>
+
+class MyGame : public gslib::Game
+{
+public:
+	void start() override
+	{
+	}
+	void update(float delta_time) override
+	{
+
+	}
+	void draw() override
+	{
+	}
+	void end() override
+	{
+
+	}
+};
+
 
 int main()
 {
-
+	return MyGame().run();
 }
