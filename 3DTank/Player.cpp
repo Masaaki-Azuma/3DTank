74eb22d4642cbc2b85337d1e2dabc5392e57c9e1
Azuma <g021g1256@g.neec.ac.jp>
2021-12-09 15:43:26 +0900

    プレイヤークラスに移動処理（ワールド空間基準）を追加

--- 3DTank/Player.cpp
+++ 3DTank/Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 
+const float MoveSpeed{ 0.6f };
 Player::Player(const GSvector3& position)
 {
 	transform_.position(position);
@@ -7,7 +8,21 @@ Player::Player(const GSvector3& position)
 
 void Player::update(float delta_time)
 {
-
+	GSvector3 direction{ 0.0f, 0.0f, 0.0f };
+	if (gsGetKeyState(GKEY_D)) {
+		direction.x += 1.0f;
+	}
+	if (gsGetKeyState(GKEY_A)) {
+		direction.x -= 1.0f;
+	}
+	if (gsGetKeyState(GKEY_W)) {
+		direction.z -= 1.0f;
+	}
+	if (gsGetKeyState(GKEY_S)) {
+		direction.z += 1.0f;
+	}
+	GSvector3 velocity = direction.normalized()* MoveSpeed* delta_time;
+	transform_.translate(velocity, GStransform::Space::World);
 }
 
 void Player::draw() const
