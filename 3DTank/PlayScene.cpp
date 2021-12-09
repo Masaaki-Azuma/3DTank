5160e6e2a7907e6356748f9e22fd08217f5e5557
Azuma <g021g1256@g.neec.ac.jp>
2021-12-09 14:07:15 +0900

    作成したスタティックメッシュ（タンク、砲丸）の動作確認 以前の2Dスプライト処理をコメントアウト

--- 3DTank/PlayScene.cpp
+++ 3DTank/PlayScene.cpp
@@ -3,7 +3,9 @@
 
 void PlayScene::start()
 {
-	gsLoadTexture(0, "Assets/test.png");
+	//gsLoadTexture(0, "Assets/test.png");
+	gsLoadMesh(0, "Assets/tank.mshb");
+	gsLoadMesh(1, "Assets/cannon_ball.mshb");
 }
 
 void PlayScene::update(float delta_time)
@@ -12,10 +14,21 @@ void PlayScene::update(float delta_time)
 
 void PlayScene::draw() const
 {
-	gsDrawSprite2D(0, NULL, NULL, NULL, NULL, NULL, 0.0f);
+	//ƒ^ƒ“ƒN‚Ì•`‰æ
+	glPushMatrix();
+	glMultMatrixf(GSmatrix4::translate(GSvector3{ 0.0f, 0.0f, 0.0f }));
+	gsDrawMesh(0);
+	glPopMatrix();
+	//–CŠÛ‚Ì•`‰æ
+	glPushMatrix();
+	glMultMatrixf(GSmatrix4::translate(GSvector3{ 0.0f, 0.0f, 20.0f }));
+	gsDrawMesh(1);
+	glPopMatrix();
 }
 
 void PlayScene::end()
 {
-	gsDeleteTexture(0);
+	//gsDeleteTexture(0);
+	gsDeleteMesh(0);
+	gsDeleteMesh(1);
 }
