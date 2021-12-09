73ae78d7407ac56ed38cbf6d65050e3f1628b647
Azuma <g021g1256@g.neec.ac.jp>
2021-12-09 17:38:22 +0900

    重力が作用するように変更

--- 3DTank/CannonBall.cpp
+++ 3DTank/CannonBall.cpp
@@ -1,5 +1,6 @@
 #include "CannonBall.h"
 
+const float Gravity{ -0.02f };
 CannonBall::CannonBall(const GSvector3& position, const GSvector3& velocity):
 	velocity_{velocity}
 {
@@ -8,7 +9,17 @@ CannonBall::CannonBall(const GSvector3& position, const GSvector3& velocity):
 
 void CannonBall::update(float delta_time)
 {
+	//d—Í‚ğì—p
+	velocity_.y += Gravity;
+	//ˆÚ“®—Ê‚ğ”½‰f
+	transform_.translate(velocity_ * delta_time);
 
+	//ForDebug
+	//‰º‚És‚Á‚½‚çíœ
+	if (transform_.position().y < -1) {
+		is_dead_ = true;
+	}
+	//!ForDebug
 }
 
 void CannonBall::draw() const
