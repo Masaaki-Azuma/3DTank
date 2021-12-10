d653658f6c6586d7db9d9b5fd53b0ed66c79068b
Azuma <g021g1256@g.neec.ac.jp>
2021-12-10 22:28:07 +0900

    境界球クラスを作成

--- 3DTank/BoundingSphere.h
+++ 3DTank/BoundingSphere.h
@@ -6,12 +6,22 @@
 class BoundingSphere
 {
 public:
-	BoundingSphere(float radius = 0.0f, const GSvector3& center = GSvector3{0.0f, 0.0f, 0.0f});
-	BoundingSphere translate(const GSvector3& position)  const;
-	bool intersects(const BoundingSphere& other);
+	BoundingSphere(float radius = 0.0f, const GSvector3& center = GSvector3{ 0.0f, 0.0f, 0.0f });
+	//平行移動
+	BoundingSphere translate(const GSvector3& position) const;
+	//変換
+	BoundingSphere transform(const GSmatrix4& matrix) const;
+	//境界球同士の衝突判定
+	bool intersects(const BoundingSphere& other) const;
+	//デバッグ用境界球描画
 	void draw() const;
+
+	float radius() const;
+	const GSvector3& center() const;
 private:
+	//半径
 	float radius_;
+	//中心座標(持ち主からの相対座標)
 	GSvector3 center_;
 };
 #endif//!BOUNDING_SPHERE_H_
