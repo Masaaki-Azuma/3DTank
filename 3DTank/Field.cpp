b8367da2551c6ce895f07a662f4e5b19f8b3bf8a
Azuma <g021g1256@g.neec.ac.jp>
2021-12-11 15:10:32 +0900

    フィールドに線分との衝突判定処理を追加

--- 3DTank/Field.cpp
+++ 3DTank/Field.cpp
@@ -1,5 +1,6 @@
 #include "Field.h"
 #include "BoundingSphere.h"
+#include "Line.h"
 #include "Assets.h"
 
 Field::Field(GSuint mesh, GSuint collider):
@@ -18,4 +19,12 @@ bool Field::collide(const BoundingSphere& sphere, GSvector3* center)
 	return gsOctreeCollisionSphere(
 		gsGetOctree(collider_),
 		&sphere.center(), sphere.radius(), center) == GS_TRUE;
+}
+
+bool Field::collide(const Line& line, GSvector3* intersect)
+{
+	return gsOctreeCollisionLine(
+		gsGetOctree(collider_),
+		&line.start(), &line.end(), intersect, nullptr
+	);
 }
--- 3DTank/Field.h
+++ 3DTank/Field.h
@@ -4,6 +4,7 @@
 #include <gslib.h>
 
 class BoundingSphere;
+class Line;
 
 //地形情報を管理するクラス
 class Field
@@ -14,6 +15,9 @@ public:
 	void draw() const;
 	//球と地形の当たり判定
 	bool collide(const BoundingSphere& sphere, GSvector3* center = nullptr);
+	//線分と地形の当たり判定
+	bool collide(const Line& line, GSvector3* intersect = nullptr);
+
 private:
 	//地形メッシュオクツリー
 	GSuint mesh_;
