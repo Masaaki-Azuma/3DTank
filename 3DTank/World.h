9d63a464249bbaa7cabd41d0168da98903ea78b6
Azuma <g021g1256@g.neec.ac.jp>
2021-12-15 23:29:28 +0900

    GUIが描画できる処理を追加

--- 3DTank/World.h
+++ 3DTank/World.h
@@ -30,13 +30,20 @@ public:
 	virtual Actor* find_actor_with_tag(const std::string& tag) const;
 	//ステージの取得
 	virtual Stage& stage() override;
+	//レベルの開始
+	virtual void change_to_battle() override;
+	//レベルの終了
+	virtual void change_to_level_end() override;
 
 private:
 	//ステージ開始状態更新処理
 	void introduction(float delta_time);
-	//ステージ戦闘状態
+	//ステージ戦闘状態更新
 	void battle(float delta_time);
-	void stage_clear(float delta_time);
+	//ステージクリア状態更新
+	void level_clear(float delta_time);
+
+	void level_end(float delta_time);
 
 private:
 	//アクター管理オブジェクト
