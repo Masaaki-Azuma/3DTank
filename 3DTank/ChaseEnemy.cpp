5609fdd09c6a2a48e812d3ca74dca2ecd92ffbe7
Azuma <g021g1256@g.neec.ac.jp>
2021-12-21 14:50:45 +0900

    追跡処理から垂直成分を除外

--- 3DTank/ChaseEnemy.cpp
+++ 3DTank/ChaseEnemy.cpp
@@ -18,13 +18,19 @@ ChaseEnemy::ChaseEnemy(IWorld* world, const GSvector3& position):
 
 void ChaseEnemy::move(float delta_time)
 {
+	//プレイヤーを検索
 	Actor* player = world_->find_actor("Player");
 	if (!player) return;
 
+	//プレイヤー方向を取得
 	GSvector3 direction = player->transform().position() - transform_.position();
 	direction.y = 0.0f;
+	//最大速度を制限
 	direction = GSvector3::clampMagnitude(direction, MoveSpeed);
-	velocity_ = GSvector3::slerp(direction, velocity_, 0.05f);
-
-	transform_.translate(velocity_ * delta_time);
+	//現在速度水平成分を取得
+	GSvector3 velocity{ velocity_.x, 0.0f, velocity_.z };
+	//目標速度と現在速度を補間
+	velocity = GSvector3::slerp(direction, velocity, 0.05f);
+	//移動量を反映
+	transform_.translate(velocity * delta_time);
 }
